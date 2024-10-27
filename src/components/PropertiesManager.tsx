925282d3b34dfb2e61efa5d0c3768f6134729de1
matrixlydev <krishnadeva@gl.block-stars.com>
2024-10-28 03:53:38 +0530

    Created the layout

--- src/components/PropertiesManager.tsx
+++ src/components/PropertiesManager.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import { cn } from '../common/utils.ts'
+import React from "react";
+import { cn } from "../common/utils.ts";
 
 export default function PropertiesManager() {
   return (
-    <div className={cn(
-	"flex m-2 bg-white/10 text-black border flex-col items-center cursor-pointer p-2 transition-colors"
-   )}>PropertiesManager</div>
-  )
+    <>
+      <div
+        className={cn(
+          "flex m-2 bg-white/10 text-black border flex-col items-center cursor-pointer p-2 transition-colors"
+        )}
+      >
+        PropertiesManager
+      </div>
+	 <div>
+		
+	 </div>
+    </>
+  );
 }
